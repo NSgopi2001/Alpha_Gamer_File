eda258ad14c91bf5ed4cb7076994734bc68aff2c
NSgopi2001 <gopishankarns2001@gmail.com>
2025-07-23 02:01:53 +0530

    Added event listeners for buttons and Identified selected card names

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,59 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Button> cardsList = new List<Button>();
+
     void Start()
     {
-        
+        LoadAllCards();
+
+        AddListeners();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadAllCards()
     {
-        
+        GameObject[] cardsArray = GameObject.FindGameObjectsWithTag("Card");
+
+        if (cardsArray.Length == 0)
+        {
+            Debug.LogWarning("No cards found with tag 'Card'. Check if your prefabs are tagged properly.");
+            return;
+        }
+
+        foreach (GameObject cards in cardsArray)
+        {
+            Button btn = cards.GetComponent<Button>();
+            if (btn != null)
+            {
+                cardsList.Add(btn);
+            }
+            else
+            {
+                Debug.LogWarning($"GameObject {cards.name} is tagged 'Card' but has no Button component.");
+            }
+        }
+
+        Debug.Log($"Loaded {cardsList.Count} cards.");
+    }
+
+
+    private void AddListeners()
+    {
+        foreach (Button cardButtons in cardsList)
+        {
+            cardButtons.onClick.AddListener(() => PickCard());
+        }
+    }
+
+    private void PickCard()
+    {
+        string cardName = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+        Debug.Log(cardName + "Button pressed");
     }
 }
