15325fe837a30174770797693fec5e5b94f1c49d
NSgopi2001 <gopishankarns2001@gmail.com>
2025-07-23 17:04:37 +0530

    Created menu with scene management and added grid dynamic using enum

--- Assets/Scripts/CardsGenerator.cs
+++ Assets/Scripts/CardsGenerator.cs
@@ -1,24 +1,68 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static GridEnum;
 
 public class CardsGenerator : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private Transform gridPanel;
     [SerializeField] private GameObject cardPrefab;
-    [SerializeField] private int numberOfCards = 6;
+
+    [Header("Layout Settings")]
+    [SerializeField] private Vector2 spacing = new Vector2(10, 10);
+    [SerializeField] private Vector2 padding = new Vector2(0, 10);
+
+    [Header("Default Grid Size")]
+    [SerializeField] private GridSize defaultGridSize = GridSize.Grid_2x3;
+
+    private GridLayoutGroup gridLayout;
+    private RectTransform panelRect;
 
     private void Awake()
     {
-        GenerateCards();
+        gridLayout = gridPanel.GetComponent<GridLayoutGroup>();
+        panelRect = gridPanel.GetComponent<RectTransform>();
+
+        SetGrid(defaultGridSize);
     }
 
-    private void GenerateCards()
+    public void SetGrid(GridSize gridSize)
     {
-        for (int i = 0; i < numberOfCards; i++)
+        int rows = 0, cols = 0;
+
+        switch (gridSize)
+        {
+            case GridSize.Grid_2x2: rows = 2; cols = 2; break;
+            case GridSize.Grid_2x3: rows = 2; cols = 3; break;
+            case GridSize.Grid_5x6: rows = 5; cols = 6; break;
+        }
+
+        if (gridLayout == null || panelRect == null)
+            return;
+
+        // Remove old cards
+        foreach (Transform child in gridPanel)
+            Destroy(child.gameObject);
+
+        // Calculate dynamic cell size
+        float width = panelRect.rect.width - padding.x * 2 - spacing.x * (cols - 1);
+        float height = panelRect.rect.height - padding.y * 2 - spacing.y * (rows - 1);
+        Vector2 cellSize = new Vector2(200, 200);
+
+        // Apply layout settings
+        gridLayout.cellSize = cellSize;
+        gridLayout.spacing = spacing;
+        gridLayout.padding = new RectOffset((int)padding.x, (int)padding.x, (int)padding.y, (int)padding.y);
+        gridLayout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        gridLayout.constraintCount = cols;
+
+        // Create cards
+        int total = rows * cols;
+        for (int i = 0; i < total; i++)
         {
             GameObject card = Instantiate(cardPrefab, gridPanel);
-            card.name = "" + i;
+            card.name = $"Card_{i}";
         }
     }
 }
---
+++ Assets/Scripts/Enum/GridEnum.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridEnum
+{
+    public enum GridSize
+    {
+        Grid_2x2,
+        Grid_2x3,
+        Grid_5x6
+    }
+
+}
